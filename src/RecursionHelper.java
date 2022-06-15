bd95ec186744b9a4adb81a2154b99b9b8549cbb0
Will Daknis <daknis@rams.colostate.edu>
2022-06-15 09:59:32 -0600

    replica of cs163 lab

---
+++ src/RecursionHelper.java
@@ -0,0 +1,29 @@
+public class RecursionHelper {
+    /** Self Explanation - Remember to detail your base and recursive cases.
+     * 
+     * 
+     * 
+     */
+    public String reverseString(String str) {
+        //TODO: Student
+        return "";
+    }
+    /** Self Explanation - Remember to detail your base and recursive cases.
+     * 
+     * 
+     * 
+     */
+    public int armstrongNumber(int num, int total) {
+        //TODO: Student
+        return -1;
+    }
+    /** Self Explanation - Remember to detail your base and recursive cases.
+     * 
+     * 
+     * 
+     */
+    public boolean palindromeChecker(String str) {
+        //TODO: Student
+        return false;
+    }
+}
